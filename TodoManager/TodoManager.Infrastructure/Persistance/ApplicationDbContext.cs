eeb0dc9aad2521462f41e5ef020914775bfbd926
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-13 09:08:11 +0300

    Configure EF core

---
+++ TodoManager/TodoManager.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using TodoManager.Application.Common.Interfaces;
+using TodoManager.Domain.Entities;
+
+namespace TodoManager.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public DbSet<TodoList> TodoLists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.DefaultConnection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ TodoManager/TodoManager.Infrastructure/Persistance/ConnectionStrings.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoManager.Infrastructure.Persistance
+{
+    public static class ConnectionStrings
+    {
+        public const string DefaultConnection = "Server=DESKTOP-U6PER2I\\SQLEXPRESS;Database=TodoManager;Trusted_Connection=True;MultipleActiveResultSets=true";
+    }
+}
