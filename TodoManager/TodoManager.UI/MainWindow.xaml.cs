513bf49ca9008b13def73dc3075a9e74c2393a95
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-04-20 22:29:39 +0300

    Create pages

--- TodoManager/TodoManager.UI/MainWindow.xaml.cs
+++ TodoManager/TodoManager.UI/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using TodoManager.UI.Views;
 
 namespace TodoManager.UI
@@ -24,17 +11,22 @@ namespace TodoManager.UI
         public MainWindow()
         {
             InitializeComponent();
-            Main.Content = new TodoLists();
+            Main.Content = new TodoItems();
         }
 
-        private void NavigateToTodoLists(object sender, RoutedEventArgs e)
+        private void NavigateToTasks(object sender, RoutedEventArgs e)
         {
-            Main.Content = new TodoLists();
+            Main.Content = new TodoItems();
         }
 
-        private void NavigateToAddTodoListPage(object sender, RoutedEventArgs e)
+        private void NavigateToMyDay(object sender, RoutedEventArgs e)
         {
-            Main.Content = new CreateTodoList();
+            Main.Content = new MyDay();
+        }
+
+        private void NavigateToImportantTasks(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new ImportantTasks();
         }
     }
 }
---
+++ TodoManager/TodoManager.UI/Views/TodoItems.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using TodoManager.UI.ViewModels;
+using static TodoManager.UI.DependencyInjection;
+
+namespace TodoManager.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for TodoItems.xaml
+    /// </summary>
+    public partial class TodoItems : Page
+    {
+        private readonly TodoItemsViewModel todoItemsViewModel;
+
+        public TodoItems()
+        {
+            InitializeComponent();
+
+            todoItemsViewModel = Resolve<TodoItemsViewModel>();
+            BindTodoItems();
+        }
+
+        private void BindTodoItems()
+        {
+            IEnumerable<Models.TodoItems.TodoItemViewModel> todoItems = todoItemsViewModel.GetTodoItems();
+            TodoItems.ItemsSource = todoItems;
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            todoItemsViewModel.AddTodoItem();
+        }
+
+        private void ToggleCompletion(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            int id = (int)button.DataContext;
+        }
+    }
+}
